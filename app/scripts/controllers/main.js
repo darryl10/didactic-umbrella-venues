6a259e2dc56ed440ea1bc2f038a36beceafe4499
Steve Brown <contactstevebrown@gmail.com>
2014-05-16 15:10:25 +0100

    Better UX

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -27,7 +27,7 @@ angular.module('ugcVizApp')
             var nodes = pack.nodes(root);
 
             for(var i = 0; i < nodes.length; ++i) {
-                setParentColor(nodes[i]);
+                setCircleColor(nodes[i]);
             }
 
             vis.selectAll("circle")
@@ -37,9 +37,9 @@ angular.module('ugcVizApp')
                 .attr("cx", function(d) { return d.x; })
                 .attr("cy", function(d) { return d.y; })
                 .attr("r", function(d) { return d.r; })
-                .on("click", function(d) { return zoom(node == d ? root : d); })
-                .attr("fill", function(d, i) { return setCircleColour(d,i)});
-//                .style("opacity", function(d) { return d.depth > 1 ? 0 : 1; })
+                .on("click", function(d, a, b) { console.log(d); return zoom(node == d ? root : d); })
+                .attr("fill", function(d, i) { return setCircleColour(d,i)})
+                .style("opacity", function(d) { return setOpacity(d)});
 
             vis.selectAll("text")
                 .data(nodes)
@@ -55,31 +55,48 @@ angular.module('ugcVizApp')
             d3.select(window).on("click", function() { zoom(root); });
         });
 
-        function setParentColor(node) {
+        function setCircleColor(node) {
             if(node.depth === 1) {
-                node.categoryColor = Math.random() * 100
-                console.log(node);
+                node.categoryColor = getRandomArbitary(0,365);
             }
         }
 
-        function setCircleColour(d,i) {
+        function setOpacity(node, rootNode, k, text) {
 
-            var color;
+            if(rootNode && rootNode.depth !== 0) {
+                if (node.depth ===1) {
+                    return 0.1
+                } else if (!text){
+                    return k * node.r > 20 ? 1 : 0;
+                }
 
-            if(d.depth === 1 ) {
-                color = d3.hsl(d.categoryColor, 0.75,0.5);
+            } else {
+                if(node.depth > 1) {
+                    return 0;
+                } else if (node.depth ===1 && !text){
+                    return 0.4;
+                } else if (node.depth ===0) {
+                    return 0;
+                }
             }
-
-            if(d.average_rating) {
-                color = d3.hsl(d.parent.categoryColor,0.75,0.5);
-                color = color.brighter(d.average_rating / 100);
-            }
-
-            return color;
 
         }
 
-        function setCircleOpacity() {
+        function getRandomArbitary (min, max) {
+            return Math.random() * (max - min) + min;
+        }
+
+        function setCircleColour(d) {
+
+                if(d.depth === 1 ) {
+                    var color = d3.hsl(d.categoryColor, 0.75,0.5);
+                    return color;
+                }
+                if(d.average_rating) {
+                    var color = d3.hsl(d.parent.categoryColor,0.75,0.5);
+                    color = color.brighter(d.average_rating / 100);
+                    return color;
+                }
 
         }
 
@@ -89,36 +106,25 @@ angular.module('ugcVizApp')
             x.domain([d.x - d.r, d.x + d.r]);
             y.domain([d.y - d.r, d.y + d.r]);
 
+            var rootNode = d;
+
             var t = vis.transition()
-                .duration(d3.event.altKey ? 7500 : 750);
+                .duration(d3.event.altKey ? 7500 : 300);
 
-            if(d.depth >= 1) {
-                t.selectAll("circle")
-                    .attr("cx", function(d) { return x(d.x); })
-                    .attr("cy", function(d) { return y(d.y); })
-                    .attr("r", function(d) { return k * d.r; })
-                    .style("opacity", function(d) { return k * d.r > 20 ? 1 : 0; });
+            t.selectAll("circle")
+                .attr("cx", function(d) { return x(d.x); })
+                .attr("cy", function(d) { return y(d.y); })
+                .attr("r", function(d) { return k * d.r; })
+                .style("opacity", function(d) { return setOpacity(d, rootNode, k) });
 
-                t.selectAll("text")
-                    .attr("x", function(d) { return x(d.x); })
-                    .attr("y", function(d) { return y(d.y); })
-                    .style("opacity", function(d) { return k * d.r > 20 ? 1 : 0; });
+            t.selectAll("text")
+                .attr("x", function(d) { return x(d.x); })
+                .attr("y", function(d) { return y(d.y); })
+                .style("opacity", function(d) { return setOpacity(d, rootNode, k, true) });
 
-                node = d;
-                d3.event.stopPropagation();
-            } else {
-                t.selectAll("circle")
-                    .attr("cx", function(d) { return x(d.x); })
-                    .attr("cy", function(d) { return y(d.y); })
-                    .attr("r", function(d) { return k * d.r; })
-                    .style("opacity", function(d) { return d.depth > 1 ? 0 : 1; });
+            node = d;
 
-                t.selectAll("text")
-                    .attr("x", function(d) { return x(d.x); })
-                    .attr("y", function(d) { return y(d.y); })
-                    .style("opacity", function(d) { return d.depth > 1 ? 0 : 1; });
-            }
-
+            d3.event.stopPropagation();
 
         }
   });
